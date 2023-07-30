b8c25a0a0bebc3ef5898a310d51a6313583f2dc9
Ishan Hegde <ihegde@connect.ust.hk>
2023-07-31 06:37:17 +0800

    Accurate wall clock

--- src/timer.c
+++ src/timer.c
@@ -4,33 +4,57 @@ struct Global_Timer* global_timer = NULL;
 
 
 void alloc_timer(char *name) {
+
     global_timer->num_timers++;
     global_timer->timers = (struct Timer*) realloc(global_timer->timers, global_timer->num_timers * sizeof(struct Timer));
     struct Timer *timer = &(global_timer->timers[global_timer->num_timers - 1]);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &timer->start_time);
     timer->name = (char*) malloc(strlen(name) + 1);
     strcpy(timer->name, name);
-    timer->start_time = clock();
-    timer->elapsed_time = 0.0;
-    timer->start_time = 0;
+    timer->elapsed_time = 0;
     timer->time_mean = 0.0;
-    timer->count = 1;
-    timer->total_elapsed_time = 0.0;
+    timer->count = 0;
+    timer->CONSTANT_1 = global_timer->resolution;
+    timer->CONSTANT_2 = global_timer->resolution/pow(10,9);
 }
 
 void watch_update(struct Timer * timer){
     
-    timer->elapsed_time = (double)(clock() - timer->total_elapsed_time) / CLOCKS_PER_SEC;
-    timer->total_elapsed_time += timer->elapsed_time;
-    timer->count++;
-    timer->time_mean = timer->total_elapsed_time/timer->count;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &timer->start_time);
+    return;
+    //timer->elapsed_time = (timer->current_time.tv_sec - timer->start_time.tv_sec) * global_timer->resolution;
+    
+    
 }
 
-void alloc_global_timer(){
+void stop_watch_(struct Timer * timer){
+    //printf("RESOLUTION: %f\n",timer->CONSTANT_2);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &timer->current_time);
+    timer->elapsed_time += (timer->current_time.tv_sec - timer->start_time.tv_sec) * timer->CONSTANT_1 + (timer->current_time.tv_nsec - timer->start_time.tv_nsec)* timer->CONSTANT_2;
+    timer->count++;
+    
+}
+
+void stop_watch(char * name){
+    
+   int i;
+    for (i = 0; i < global_timer->num_timers; i++) {
+        if (strcmp(global_timer->timers[i].name, name) == 0){
+            
+            stop_watch_(&global_timer->timers[i]);
+            return;
+        }
+    }
+}
+
+
+void alloc_global_timer(enum TIME_RESOLUTION resolution){
 
     global_timer = (struct Global_Timer *)malloc(sizeof(struct Global_Timer));
     global_timer->num_timers =0;
     global_timer->timers =NULL;
     global_timer->total_time =0.0;
+    global_timer->resolution = resolution;
 }
 
 
@@ -47,7 +71,7 @@ void watch(char * name){
     }
     
     alloc_timer(name);
-    
+    return;
 }
 
 void free_global_timer(){
@@ -63,9 +87,10 @@ void free_global_timer(){
 void print_statistics(){
 
     for (int i=0;i<global_timer->num_timers;i++){
+        global_timer->timers[i].time_mean =(double) global_timer->timers[i].elapsed_time/global_timer->timers[i].count;
         printf("%s: \n",global_timer->timers[i].name);
-        printf("Total Time: %f\n",global_timer->timers[i].total_elapsed_time);
-        printf("Average Time: %f\n",global_timer->timers[i].time_mean);
+        printf("Total Time: %ld\n",global_timer->timers[i].elapsed_time);
+        printf("Average Time: %.1f\n",global_timer->timers[i].time_mean);
         printf("Count: %d\n", global_timer->timers[i].count);
     }
 }
--- src/timer.h
+++ src/timer.h
@@ -4,7 +4,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+//#include <sys/time.h>
+#include <stdint.h>
+#include <math.h>
 
+
+#define WATCH(name) watch(name);
+
+enum TIME_RESOLUTION{
+    SECONDS         =         1,
+    MILLISECONDS    =         1000,
+    MICROSECONDS    =         1000000,
+    NANOSECONDS     =         1000000000
+};
 
 struct Timer;
 
@@ -13,27 +26,34 @@ struct Global_Timer{
     struct Timer * timers;
     double total_time;
     size_t num_timers;
+    enum TIME_RESOLUTION resolution;
 };
 
 extern struct Global_Timer* global_timer;
 
 struct Timer{
-    clock_t start_time;
-    double elapsed_time;
-    double total_elapsed_time;
+    struct timespec start_time;
+    struct timespec current_time;
+    uint64_t elapsed_time;
     char * name;
     double time_mean;
     int count;
+    uint64_t CONSTANT_1;
+    double CONSTANT_2;
 };
+
 
 
 
 void watch_update(struct Timer * timer);
 
+void stop_watch_(struct Timer * timer);
+
+void stop_watch(char * name);
 
 void alloc_timer(char * name);
 
-void alloc_global_timer();
+void alloc_global_timer(enum TIME_RESOLUTION resolution);
 
 void watch(char * name);
 
@@ -43,8 +63,8 @@ void print_statistics();
 
 void exit_functions();
 
-#define GLOBAL_TIMER() \
-    alloc_global_timer(); \
+#define GLOBAL_TIMER(resolution) \
+    alloc_global_timer(resolution); \
     atexit(exit_functions);\
 
 #endif 
